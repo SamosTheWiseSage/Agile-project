ced52abd1740c4359a0986304e6676402f7ea6bc
SamosTheWiseSage <GE.kelemwork@gmail.com>
2024-11-25 18:16:58 +0100

    Kinda fixed it can run now
    better now it can run but i may have lost track of what i changed gonna try and backtrack and fix this now

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Contact from "./components/Contact";
-import FAQs from "./components/FAQs";
-import Home from "./components/Home";
+// src/App.js
+import React from 'react';
+import Contact from './components/Contact';
+import FAQs from './components/FAQs';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
+    <div className="App">
+      <h1>Welcome to the React App!</h1>
       <Contact />
       <FAQs />
     </div>
