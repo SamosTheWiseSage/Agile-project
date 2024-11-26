340f54426bd25cb1fd299f00faa997c99d66c3d7
Erik Werther <erik.werther@gmail.com>
2024-11-26 15:50:02 +0100

    blockera drop down meny, kan lägga till igen senare

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,16 +5,6 @@ import Layout from './Layout';
 const Home = () => {
   return (
     <Layout>
-      <div className="relative w-full h-80 md:h-[500px] lg:h-[700px]">
-        <div className="absolute inset-0 flex w-full h-full">
-          {/* Här skulle du ha en bildslider */}
-        </div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-white text-4xl md:text-6xl lg:text-8xl font-bold drop-shadow-lg">
-            Make a statement out of yourself
-          </h1>
-        </div>
-      </div>
 
       <div className="text-center mt-12">
         <h2 className="text-3xl md:text-4xl font-extrabold text-gray-800">Explore Our Exclusive Offerings</h2>
